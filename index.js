a1bdf436db3174a839a5a57373afaa9436b670ab
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 12:31:42 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// 1. Remove the main element from the DOM
+const main = document.getElementById("main");
+main.remove();
+
+// 2. Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// 3. Assign an id of "victory" to the newHeader
+newHeader.id = "victory";
+
+// 4. Set the text content of the newHeader to "YOUR-NAME is the champion"
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// 5. Append the newHeader to the document body
+document.body.appendChild(newHeader);
